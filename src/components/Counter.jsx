221a312a33b26528986de0953d94779d3a76bb88
GabrielaDana <gabriela.dana.pacheco@gmail.com>
2022-06-07 22:34:07 -0400

    prueba deploy

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,6 +3,6 @@ import style from './assets/Counter.module.css'
 
 export const Counter = () => {
   return (
-    <h1 className={style.title}> Hola mundo </h1>
+    <h1 className={style.title}> Hola mundo !!</h1>
   )
 }
